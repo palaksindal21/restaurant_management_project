6c285032d56da2a02457e44c031da4e8325ada2d
palaksindal21 <168723440+palaksindal21@users.noreply.github.com>
2025-08-06 21:13:13 +0530

    20828

--- account/models.py
+++ account/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    phone_number = models.CharField(max_length=15,blank=True,null=True)
 
     def __str__(self):
         return self.name
