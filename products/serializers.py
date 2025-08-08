3f8338cbf61a01bb9495e05deddc3b56d8ef1968
palaksindal21 <168723440+palaksindal21@users.noreply.github.com>
2025-08-08 19:37:20 +0530

    20832

--- products/serializers.py
+++ products/serializers.py
@@ -5,3 +5,8 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+class MenuItem(serializers.Modelserializer):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
