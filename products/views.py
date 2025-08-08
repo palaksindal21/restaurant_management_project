85b29911471075c326b3c72a3324ecc9b05e4a51
palaksindal21 <168723440+palaksindal21@users.noreply.github.com>
2025-08-08 19:59:16 +0530

    20832

--- products/views.py
+++ products/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from .models import Item
 from .models import Menu
-from .serializers import MenuItem
+from .serializers import MenuItemSerializer
 from .serializers import ItemSerializer
 
 '''
@@ -26,3 +25,18 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MenuView(APIView): 
+    # GET - retrieve all menu
+    def get(self, request):
+        menu_item = Menu.objects.all() # fetch all menu items from database
+        serializer = MenuItemSerializer(menu_item, many=True)
+        return response(serializer.data, status=status.HTTP_200_OK)
+
+    # POST add a new menu item
+    def post(self, request):
+        serializer = MenuItemSerializer(data=request.data) # Take JSON from request
+        if serializer.is_valid(): # validate incoming data
+            serializer.save() # save item to database
+            return response(serializer.data, status=status.HTTP_201_CREATED)
+        return response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
