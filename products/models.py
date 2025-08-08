db170e206a929b5948ab005bda652f00adf3ed4f
palaksindal21 <168723440+palaksindal21@users.noreply.github.com>
2025-08-08 19:44:58 +0530

    20832

--- products/models.py
+++ products/models.py
@@ -12,4 +12,7 @@ class Item(models.Model):
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(max_length=)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return str()
